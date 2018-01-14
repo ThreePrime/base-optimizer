b1b2a2a7b01606581e249d66b03d856303812c37
Zach Deibert <zachdeibert@gmail.com>
2018-01-14 13:34:54 -0600

    Created project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Com.GitHub.ThreePrime.BaseOptimizer {
+    static class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
